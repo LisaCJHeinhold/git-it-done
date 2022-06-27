b22f59c12fe7b5e3eeb0b8c6ed6f5a8c34189f7a
Lisa <lisacjheinhold@gmail.com>
2022-06-27 11:10:14 -0600

    finished project

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,4 @@
+var languageButtonsEl = document.querySelector("#language-buttons");
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
@@ -71,5 +72,27 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 };
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch (apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    });
+};
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+        console.log(language);
+        if (language) {
+            getFeaturedRepos(language);
+            //clear old content
+            repoContainerEl.textContent = "";
+        }
+};
 
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formSubmitHandler);
